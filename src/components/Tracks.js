10dd4f7895844f2476160b50c3ddd703b7fa5e79
Jacob Colyvan <jacob_colyvan@live.com>
2020-09-01 21:52:44 +1000

    added popularity rating to Tracks

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -11,42 +11,61 @@ const Tracks = ({ tracks, artistData, audioFeatures }) => {
   return (
     <div>
       <ul>
-        {(tracks) && tracks.map((track, index) => (
-          <Accordion 
-          key={`artist${index}`} 
-          variant='outlined'
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls='panel1a-content'
-              id='track-header'
-            >
-              <Typography>{track.name} – <i>{track.artists[0].name}</i></Typography>
-            </AccordionSummary>
-
-            {artistData && (
-              <AccordionDetails style={{ padding: '8px 16px 0 16px' }}>
-                <Typography variant='body2'>
-                  <li><b>Genres:</b> {artistData[index].data.genres.join(', ')}.</li>   
+        {tracks &&
+          tracks.map((track, index) => (
+            <Accordion key={`artist${index}`} variant='outlined'>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel1a-content'
+                id='track-header'
+              >
+                <Typography>
+                  {track.name} – <i>{track.artists[0].name}</i>
                 </Typography>
-              </AccordionDetails>
-            )}
-                      
-            {audioFeatures && (
-              <AccordionDetails style={{ padding: '0 16px 16px 16px' }}>
-                <ul>
-                  {Object.keys(audioFeatures[index]).map((feature, index2) => (
-                    (audioFeatures[index] && ( 
-                    <li className='feature average' key={`feature${index2}`}>
-                      <b><i>{feature}:</i></b> {audioFeatures[index][feature]}
-                    </li>
-                    )
-                  )))}
-                </ul>
-              </AccordionDetails>
-            )}
-          </Accordion>
-        ))}
+              </AccordionSummary>
+
+              {artistData &&
+                (artistData[index].data.genres.length > 0 && (
+                  <AccordionDetails style={{ padding: '8px 16px 0 16px' }}>
+                    <Typography variant='body2'>
+                      <li>
+                        <b>Genres:</b>{' '}
+                        {artistData[index].data.genres.join(', ')}.
+                      </li>
+                    </Typography>
+                  </AccordionDetails>
+                ),
+                (
+                  <AccordionDetails>
+                    <Typography variant='body2'>
+                      <b>Artist Popularity Rating:</b>{' '}
+                      {artistData[index].data.popularity}
+                    </Typography>
+                  </AccordionDetails>
+                ))}
+
+              {audioFeatures && (
+                <AccordionDetails style={{ padding: '0 16px 16px 16px' }}>
+                  <ul>
+                    {Object.keys(audioFeatures[index]).map(
+                      (feature, index2) =>
+                        audioFeatures[index] && (
+                          <li
+                            className='feature average'
+                            key={`feature${index2}`}
+                          >
+                            <b>
+                              <i>{feature}:</i>
+                            </b>{' '}
+                            {audioFeatures[index][feature]}
+                          </li>
+                        )
+                    )}
+                  </ul>
+                </AccordionDetails>
+              )}
+            </Accordion>
+          ))}
       </ul>
     </div>
   );
